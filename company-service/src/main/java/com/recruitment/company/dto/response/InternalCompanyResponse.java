52d05e96fe43e33d2dab0867ab5ea646727478a9
kimo <149oanh@gmail.com>
2026-08-11 15:42:14 +0700

    Add current project implementation

--- company-service/src/main/java/com/recruitment/company/dto/response/InternalCompanyResponse.java
+++
@@ -1,9 +0,0 @@
-package com.recruitment.company.dto.response;
-
-/** Used by other services (Job, ATS) to resolve basic company info. */
-public record InternalCompanyResponse(
-        Long id,
-        String name,
-        String logoUrl,
-        boolean verified
-) {}
