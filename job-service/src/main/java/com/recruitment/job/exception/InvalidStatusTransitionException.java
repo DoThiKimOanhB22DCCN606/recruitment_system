35903ca35d5ed680e7ceb5972dbfc4c1a8d4fa28
Đỗ Thị Kim Oanh <132663049+DoThiKimOanhB22DCCN606@users.noreply.github.com>
2026-08-14 15:21:59 +0700

    Delete job-service directory

--- job-service/src/main/java/com/recruitment/job/exception/InvalidStatusTransitionException.java
+++
@@ -1,7 +0,0 @@
-package com.recruitment.job.exception;
-
-public class InvalidStatusTransitionException extends RuntimeException {
-    public InvalidStatusTransitionException(String message) {
-        super(message);
-    }
-}
--- job-service/src/main/java/com/recruitment/job/repository/JobCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.recruitment.job.repository;
-
-import com.recruitment.job.entity.JobCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobCategoryRepository extends JpaRepository<JobCategory, Long> {
-}
